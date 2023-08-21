de79f9a3a6f8946e025a7f1e3bd2b1390a94da9c
Flynn <wphestiraid@gmail.com>
2023-08-22 08:39:14 +0900

    :white_check_mark: 쓰레드 로컬(ThreadLocal) - ThreadLocal: 예제 코드

---
+++ src/test/java/dev/flynnpark/springcoreadvanced/trace/treadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,37 @@
+package dev.flynnpark.springcoreadvanced.trace.treadlocal;
+
+import dev.flynnpark.springcoreadvanced.trace.treadlocal.code.FieldService;
+import dev.flynnpark.springcoreadvanced.trace.treadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> service.logic("userA");
+        Runnable userB = () -> service.logic("userB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("threadA");
+        Thread threadB = new Thread(userB);
+        threadB.setName("threadB");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(3000);
+        log.info("main end");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
