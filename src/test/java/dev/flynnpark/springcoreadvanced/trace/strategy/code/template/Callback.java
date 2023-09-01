d15f74f96228626006cb9316b9f87bda1720fe95
Flynn <wphestiraid@gmail.com>
2023-09-01 09:02:22 +0900

    :sparkles: 템플릿 메서드 패턴과 콜백 패턴 - 템플릿 콜백 패턴: 예제

---
+++ src/test/java/dev/flynnpark/springcoreadvanced/trace/strategy/code/template/Callback.java
@@ -0,0 +1,3 @@
+package dev.flynnpark.springcoreadvanced.trace.strategy.code.template;public interface Callback {
+    void call();
+}
---
+++ src/test/java/dev/flynnpark/springcoreadvanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -0,0 +1,15 @@
+package dev.flynnpark.springcoreadvanced.trace.strategy.code.template;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeLogTemplate {
+    public void execute(Callback callback) {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        callback.call(); // 위임
+        // 비즈니스 로직 종료
+        long resultTime = System.currentTimeMillis() - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+}
