4eb2649e157fd77f885a3e4e6f0cb296ed911ac2
Flynn <wphestiraid@gmail.com>
2023-11-20 08:36:34 +0900

    :white_check_mark: 스프링이 지원하는 프록시 - 예제 코드 1: 어드바이저

---
+++ src/test/java/dev/flynnpark/springcoreadvanced/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,24 @@
+package dev.flynnpark.springcoreadvanced.proxy.advisor;
+
+import dev.flynnpark.springcoreadvanced.proxy.common.advice.TimeAdvice;
+import dev.flynnpark.springcoreadvanced.proxy.common.service.ServiceImpl;
+import dev.flynnpark.springcoreadvanced.proxy.common.service.ServiceInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class AdvisorTest {
+    @Test
+    void advisorTest() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.find();
+        proxy.save();
+    }
+}
