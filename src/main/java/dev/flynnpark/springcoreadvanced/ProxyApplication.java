8ccdda087a3096316e68e7e9b34b9b117826d853
Flynn <wphestiraid@gmail.com>
2023-11-21 08:58:45 +0900

    :sparkles: 스프링이 지원하는 프록시 - 프록시 팩토리: 적용 1

--- src/main/java/dev/flynnpark/springcoreadvanced/ProxyApplication.java
+++ src/main/java/dev/flynnpark/springcoreadvanced/ProxyApplication.java
@@ -1,14 +1,15 @@
 package dev.flynnpark.springcoreadvanced;
 
-import dev.flynnpark.springcoreadvanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import dev.flynnpark.springcoreadvanced.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import dev.flynnpark.springcoreadvanced.trace.logtrace.LogTrace;
 import dev.flynnpark.springcoreadvanced.trace.logtrace.ThreadLocalLogTrace;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(scanBasePackages = "dev.flynnpark.springcoreadvanced.app.v1")
 public class ProxyApplication {
 
