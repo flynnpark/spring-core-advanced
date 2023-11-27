e8821d75e025daadac7b82dafb724021beb0ad91
Flynn <wphestiraid@gmail.com>
2023-11-27 09:02:03 +0900

    :sparkles: 빈 후처리기 - 스프링이 제공하는 빈 후처리기 2

--- src/main/java/dev/flynnpark/springcoreadvanced/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/dev/flynnpark/springcoreadvanced/config/v5_autoproxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import dev.flynnpark.springcoreadvanced.config.v3_proxyfactory.advice.LogTraceAd
 import dev.flynnpark.springcoreadvanced.trace.logtrace.LogTrace;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,26 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace trace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* dev.flynnpark.springcoreadvanced.app..*(..))");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* dev.flynnpark.springcoreadvanced.app..*(..)) && !execution(* dev.flynnpark.springcoreadvanced.app..noLog(..))");
         LogTraceAdvice advice = new LogTraceAdvice(trace);
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
